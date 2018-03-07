de5128a0134051c68eec8eb52fca51ee4898f474
hadxu <hadxu123@gmail.com>
2018-03-07 10:50:58 +0800

    cuda

--- thunder/nn/__init__.py
+++ thunder/nn/__init__.py
@@ -14,5 +14,6 @@ __author__ = 'haxu'
 
 from .activations import relu
 from .activations import sigmoid
-from .activations import softmax_func
+from .activations import softmax
+from .utils import softmax_func
 from .loss_functions import softmax_cross_entropy_with_logits
